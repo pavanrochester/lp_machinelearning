bc3d0a4fedae84cb2196fb50b91ad1ca9f794125
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-26 02:38:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ with st.expander('Data'):
   st.write('**Y**')
   Y=df.species
   Y
+
+with st.expander('data Visualisation'):
+  st.scatter_chart(data='df',x='bill_length',y='body_mass_g',color='species')
