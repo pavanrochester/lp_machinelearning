aa7ae5d72dd90a78fede4fb5c555f17657ea3ddb
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-26 03:04:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ input_df=pd.DataFrame(data,index=[0])
 
 input_penguins=pd.concat([input_df,X],axis=0)
 
-with st.expander('Input Features'):
+with st.expander('**Input Features**'):
   st.write('**Input penguin**')
   input_df
   st.write('**Combined Date**')
