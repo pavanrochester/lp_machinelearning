27f3829ca4c3ded8e18b54f6b98e2eb67b90e2d6
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-26 03:03:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,11 +40,15 @@ data={'island':island,
       'flipper_length_mm':flipper_length_mm,
       'body_mass_g':body_mass_g}
 input_df=pd.DataFrame(data,index=[0])
-input_df
+
 input_penguins=pd.concat([input_df,X],axis=0)
 
 with st.expander('Input Features'):
   st.write('**Input penguin**')
+  input_df
+  st.write('**Combined Date**')
+  input_penguins
+  
       
   
   
