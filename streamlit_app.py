d379c27ab336374bd157565ebb0e78e2723ac6c7
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-25 21:48:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ st.write('**Raw Data**')
 
 df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
+
+st.write('**X**')
+X=df.drop('Species',axis=1)
+X
