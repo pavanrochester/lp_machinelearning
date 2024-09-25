2783800edb03b73329715fa3692056e7e43149dc
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-26 03:01:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,9 @@ data={'island':island,
 input_df=pd.DataFrame(data,index=[0])
 input_df
 input_penguins=pd.concat([input_df,X],axis=0)
+
+with st.expander('Input Features'):
+  st.write('Input Features')
       
   
   
