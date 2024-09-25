349ba1efaa28faa4e4a5a80c85be0d0e8ae2adc2
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-25 21:46:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('🎈 App Name')
 st.info('This is a machine learning app')
-st.write('Hello world!')
+st.write('**Raw Data')
 
 df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
