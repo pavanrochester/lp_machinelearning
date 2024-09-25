4c47eb79b78d13a421a5e4032d878bc75347f6eb
Lanke Pavan Kumar <30333693+pavanrochester@users.noreply.github.com>
2024-09-25 21:52:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,15 @@ st.title('🎈 App Name')
 st.info('This is a machine learning app')
 st.write('**Raw Data**')
 
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
 
-st.write('**X**')
-X=df.drop('species',axis=1)
-X
-
-st.write('**Y**')
-Y=df.species
-Y
+with st.expander('Data'):
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+  
+  st.write('**X**')
+  X=df.drop('species',axis=1)
+  X
+  
+  st.write('**Y**')
+  Y=df.species
+  Y
